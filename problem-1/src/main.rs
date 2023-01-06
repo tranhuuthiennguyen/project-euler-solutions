c11251d8c282109e9c67b4a3369c60c6ff72ddf4
tranhuuthiennguyen <kevinnguyen31415@gmail.com>
2023-01-06 13:49:51 +0700

    problem 1 solved

--- problem-1/src/main.rs
+++ problem-1/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sum = 0;
+    let num = 1000;
+    for i in 0..num {
+        if i%3 == 0 || i%5==0 {
+            sum+=i;
+        }
+    }
+    println!("{sum}");
 }
